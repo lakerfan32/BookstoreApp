64b479fc432ed77c86f6fb0e4c94d5a8b764e1b4
RalphM <36464444+lakerfan32@users.noreply.github.com>
2018-09-24 23:28:59 -0400

    -Add a CursorLoader and set it up so that it automatically loads

--- app/src/main/java/com/udacity/ralph/bookstoreapp/CatalogActivity.java
+++ app/src/main/java/com/udacity/ralph/bookstoreapp/CatalogActivity.java
@@ -1,7 +1,10 @@
 package com.udacity.ralph.bookstoreapp;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,7 +20,11 @@ import com.udacity.ralph.bookstoreapp.data.BookContract.BookEntry;
 /**
  * Displays list of books that were entered and stored in the app.
  */
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int BOOK_LOADER = 0;
+
+    BookCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,48 +46,14 @@ public class CatalogActivity extends AppCompatActivity {
         // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
         View emptyView = findViewById(R.id.empty_view);
         bookListView.setEmptyView(emptyView);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new BookCursorAdapter(this, null);
+        bookListView.setAdapter(mCursorAdapter);
 
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the books database.
-     */
-    private void displayDatabaseInfo() {
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                BookEntry._ID,
-                BookEntry.COLUMN_PRODUCT_NAME,
-                BookEntry.COLUMN_PRICE,
-                BookEntry.COLUMN_QUANTITY,
-                BookEntry.COLUMN_SUPPLIER_NAME,
-                BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER,
-                BookEntry.COLUMN_CATEGORY,
-                BookEntry.COLUMN_GENRE };
-
-        // Perform a query on the provider using the ContentResolver.
-        // Use the {@link BookEntry#CONTENT_URI} to access the book data.
-        Cursor cursor = getContentResolver().query(
-                BookEntry.CONTENT_URI,   // The content URI
-                projection,              // The columns to return for each row
-                null,            // Selection criteria
-                null,         // Selection criteria
-                null);          // The sort order for the returned rows
-
-        // Find the ListView which will be populated with the book data
-        ListView bookListView = (ListView) findViewById(R.id.list);
-
-        // Setup an Adapter to create a list item for each row of book data in the Cursor.
-        BookCursorAdapter adapter = new BookCursorAdapter(this, cursor);
-
-        // Attach the adapter to the ListView.
-        bookListView.setAdapter(adapter);
+        // Kick off the loader
+        getLoaderManager().initLoader(BOOK_LOADER, null, this);
     }
 
     /**
@@ -121,7 +94,6 @@ public class CatalogActivity extends AppCompatActivity {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
                 insertBook();
-                displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
@@ -129,5 +101,39 @@ public class CatalogActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_PRODUCT_NAME,
+                BookEntry.COLUMN_PRICE,
+                BookEntry.COLUMN_QUANTITY,
+                BookEntry.COLUMN_SUPPLIER_NAME,
+                BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER,
+                BookEntry.COLUMN_CATEGORY,
+                BookEntry.COLUMN_GENRE };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,      // Parent activity context
+                BookEntry.CONTENT_URI,            // Provider content URI to query
+                projection,                       // Columns to include in the resulting Cursor
+                null,                     // No selection clause
+                null,                  // No selection arguments
+                null);                   // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update {@link BookCursorAdapter} with this new cursor containing updated book data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
